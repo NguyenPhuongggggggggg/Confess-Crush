df1d055c062c6ff9abd9544dff8e9b7d8caa19d7
NguyenPhuongggggggggg <96119876+NguyenPhuongggggggggg@users.noreply.github.com>
2021-12-15 17:03:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Trang heooo",
-    introTitle: 'Trang dễ thương',
+    titleWeb: "crush",
+    introTitle: 'Gui crush',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao mi cứ đi nhầm đường
-    Lạc vào tim t lẻ loi
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'Kick vào đi',
-    title: 'Giờ mi có yêu t ko hảaaaaa',
+    title: 'Giờ em có yêu anh ko hảaaaaa',
     desc: 'Yêu thì yêu ko yêu thì cũng phải yêu nhá',
-    btnYes: 'Yêu mi',
+    btnYes: 'Yêu anh',
     btnNo: 'Nằm mơ đi',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao mi lại yêu t <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'love you',
-    mess: 'T biết mà 🥰. Yêu mi nhiều nhiều 😘😘',
-    messDesc: 'Tết ra t dẫn đi chơi nhá',
+    mess: 'anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'tối 7h a qua đón đi chơi nhá',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
